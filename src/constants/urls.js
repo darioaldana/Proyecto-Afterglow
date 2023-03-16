5ef238a785a111d28f4c847976587d1d3396f17a
Simon Meza <simon.meza@correo.unimet.edu.ve>
2023-03-15 23:47:36 -0400

    Listo el menu del startpage

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,4 +1,10 @@
 export const HOME_URL = "/";
 export const REGISTER_URL = "/registro";
 export const LOGIN_URL = "/login";
-export const USER_PAGE = "/profile";
+export const USER_PAGE = "/startpage";
+export const CHAT_PAGE = "/chat";
+export const DATE_PAGE = "/dates";
+export const CATALOG_PAGE = "/catalogo";
+export const REVIEW_PAGE = "/review";
+export const PROFILE_PAGE = "/profile";
+
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 // import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage'
-import {HOME_URL, LOGIN_URL, REGISTER_URL, USER_PAGE,} from "./constants/urls";
+import {CHAT_PAGE, HOME_URL, LOGIN_URL, REGISTER_URL, USER_PAGE, REVIEW_PAGE,CATALOG_PAGE,PROFILE_PAGE,DATE_PAGE } from "./constants/urls";
 import './index.css'
 import UserPage from './pages/UserPage/UserPage';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +17,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path={REGISTER_URL} element={<RegisterPage/>}/>
         <Route path={LOGIN_URL} element={<LoginPage/>}/>
         <Route path={USER_PAGE} element={<UserPage/>}/>
+        <Route path={CHAT_PAGE} element={<h1>CHAT PAGE</h1>}/>
+        <Route path={REVIEW_PAGE} element={<h1>REVIEW PAGE</h1>}/>
+        <Route path={CATALOG_PAGE} element={<h1>CATALOG PAGE</h1>}/>
+        <Route path={PROFILE_PAGE} element={<h1>PROFILE PAGE</h1>}/>
+        <Route path={DATE_PAGE} element={<h1>DATE PAGE</h1>}/>
+        
+
+
+        
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
