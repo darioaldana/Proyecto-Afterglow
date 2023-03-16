ad5184bd8e5cfdbc0e4892c480a1287a7ec12862
Dario <dario.aldana@correo.unimet.edu.ve>
2023-03-16 04:41:19 -0400

    Componente de cartas de doctores para el catalogo (los doctores escritos en codigo)

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,7 +4,7 @@ export const LOGIN_URL = "/login";
 export const USER_PAGE = "/startpage";
 export const CHAT_PAGE = "/chat";
 export const DATE_PAGE = "/dates";
-export const CATALOG_PAGE = "/catalogo";
+export const CATALOG_PAGE = "/catalog";
 export const REVIEW_PAGE = "/review";
 export const PROFILE_PAGE = "/profile";
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import UserPage from './pages/UserPage/UserPage';
+import CatalogPage from "./pages/CatalogPage/CatalogPage"
 import {CHAT_PAGE, HOME_URL, LOGIN_URL, REGISTER_URL, USER_PAGE, REVIEW_PAGE,CATALOG_PAGE,PROFILE_PAGE,DATE_PAGE } from "./constants/urls";
 import './index.css'
 
@@ -18,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path={USER_PAGE} element={<UserPage/>}/>
         <Route path={CHAT_PAGE} element={<h1>CHAT PAGE</h1>}/>
         <Route path={REVIEW_PAGE} element={<h1>REVIEW PAGE</h1>}/>
-        <Route path={CATALOG_PAGE} element={<h1>CATALOG PAGE</h1>}/>
+        <Route path={CATALOG_PAGE} element={<CatalogPage/>}/>
         <Route path={PROFILE_PAGE} element={<h1>PROFILE PAGE</h1>}/>
         <Route path={DATE_PAGE} element={<h1>DATE PAGE</h1>}/>
         
