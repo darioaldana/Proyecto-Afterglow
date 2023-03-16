14f7d2495c81c9b1208f4e8d4804fc5f7244c901
Dario <dario.aldana@correo.unimet.edu.ve>
2023-03-16 03:45:43 -0400

    Navbar actualizacion a funcion con default

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,29 +2,25 @@ import { Component } from "react";
 import "./NavbarStyles.css";
 import { Link, useNavigate } from "react-router-dom";
 
-class Navbar extends Component {
-    render(){
-        return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Afterglow</h1>
-                <ul className="nav-menu">
-                        <li>
-                        <Link to = "/catalog">
-                            <button className="nav-links">Catalog</button>
-                        </Link>
-
-
-                        <Link to="/about">
-                            <button  className="nav-links">
-                                About
-                            </button>
-                        </Link>
-                        </li>
-                       
-                </ul>
-            </nav>
-        )
-    }
+function Navbar() {
+    return(<>
+        <nav className="NavbarItems">
+            <h1 className="navbar-logo">Afterglow</h1>
+            <ul className="nav-menu">
+                    <li>
+                    <Link to = "/catalog">
+                        <button className="nav-links">Catalog</button>
+                    </Link>
+                    <Link to="/about">
+                        <button  className="nav-links">
+                            About
+                        </button>
+                    </Link>
+                    </li>
+            </ul>
+        </nav>
+        </>
+    )
 }
 
 export default Navbar;
