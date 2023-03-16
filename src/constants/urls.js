25de903d4916d1d3c6196bbe3141816362e97802
Dario <dario.aldana@correo.unimet.edu.ve>
2023-03-16 02:56:13 -0400

    Ensamble de las paginas de inicio, login y registro

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,5 @@
 export const HOME_URL = "/";
-export const REGISTER_URL = "/registro";
+export const REGISTER_URL = "/register";
 export const LOGIN_URL = "/login";
 export const USER_PAGE = "/startpage";
 export const CHAT_PAGE = "/chat";
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-// import HomePage from './pages/HomePage/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import {CHAT_PAGE, HOME_URL, LOGIN_URL, REGISTER_URL, USER_PAGE, REVIEW_PAGE,CATALOG_PAGE,PROFILE_PAGE,DATE_PAGE } from "./constants/urls";
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <BrowserRouter>
       <Routes>
-        <Route path={HOME_URL} element={<h1>HOME PAGE</h1>}/>
+        <Route path={HOME_URL} element={<HomePage/>}/>
         <Route path={REGISTER_URL} element={<RegisterPage/>}/>
         <Route path={LOGIN_URL} element={<LoginPage/>}/>
         <Route path={USER_PAGE} element={<UserPage/>}/>
