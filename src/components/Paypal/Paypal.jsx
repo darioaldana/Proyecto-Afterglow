1687c9ff7478ca7f939138ebd39014fe5682ec54
Gabriel <gabriel.coelho@correo.unimet.edu.ve>
2023-03-25 19:24:13 -0400

    Componente Paypal (Comentado)

--- src/components/Paypal/Paypal.jsx
+++ src/components/Paypal/Paypal.jsx
@@ -1,5 +1,9 @@
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
+//Se llama la funcion y se le pasa de parametro el precio de la consulta
+//Por ejemplo: {paypal("15.99")} el numero debe tener punto y dos decimales
+//El usuario es: gabriel.coelho@correo.unimet.edu.ve
+//La contraseña es: Afterglow
 function Paypal(precio_consulta){
     return(
         <PayPalScriptProvider options={{"client-id": "AZNmzJqJ5XDBRUiSn-2gy8pvMxsbshgyFJHnOrD8-ofQ72Fn00z9GGtg6qnmf4MfTGS6MZ2Lx2Lh5zhf"}}>
