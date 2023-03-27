8f87ef2b5552b4f44c8e46edf1fa4c9e5b54de49
RoyMartin <ranseartin@gmail.com>
2023-03-27 19:39:46 -0400

    Alineación de la Navbar

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -4,7 +4,9 @@ import Chat from "../../components/Chating/Chat";
 
 function ChatPage() {
   return (
-    <div className="bg-azul h-screen flex items-center justify-center">
+    <div className="bg-turquesa-3 h-screen flex items-center justify-center flex-col">
+      <div className="h-20">
+      </div>
       <div className="border border-solid border-white rounded-lg w-4/6 h-4/5 flex overflow-hidden">
         <Sidebar />
         <Chat />
