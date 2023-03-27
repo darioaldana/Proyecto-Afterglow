b20d2739fe9a9286e8dfcea61cb47c9e56de9aa6
Dario <dario.aldana@correo.unimet.edu.ve>
2023-03-27 16:01:27 -0400

    Pagina de citas a medias

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,8 +4,9 @@ export const REGISTER_URL2 = "/register/2";
 export const LOGIN_URL = "/login";
 export const USER_PAGE = "/startpage";
 export const CHAT_PAGE = "/chat";
-export const DATE_PAGE = "/dates";
+export const APPOINTMENTS_PAGE = "/appointments";
 export const CATALOG_PAGE = "/catalog";
 export const REVIEW_PAGE = "/review";
 export const PROFILE_PAGE = "/profile";
+
 
