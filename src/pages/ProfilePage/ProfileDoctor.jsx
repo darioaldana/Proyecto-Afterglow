178c9357ea2f3c0cf662278e431bab221cdf8b69
RoyMartin <ranseartin@gmail.com>
2023-03-28 00:28:07 -0400

    Reparacion de CSS

--- src/pages/ProfilePage/ProfileDoctor.jsx
+++ src/pages/ProfilePage/ProfileDoctor.jsx
@@ -13,41 +13,41 @@ function ProfileDoctor() {
             <div className="Datos">
                 <img className={styles.profileImage} src="{perfil}" alt="Imagen de perfil"></img> 
                 <div className="datosEscritos">
-                    <form>
+                    <form className='form'>
                         <div className={styles.rectangleData} id='name'>
                             <button placeholder='Nombre: '/> 
                             <label className="m-2 font-bold">Nombre: </label>
-                            <input className="m-2 input p-1" type="text"></input>
+                            <input className="m-2 input p-1 input" type="text"></input>
                         </div>
                         <div className={styles.rectangleData} id='lastname'>
                             <button placeholder='Apellido: '/> 
                             <label className="m-2 font-bold">Apellido: </label>
-                            <input className="m-2 input p-1" type="text"></input>
+                            <input className="m-2 input p-1 input" type="text"></input>
                         </div>
                         <div className={styles.rectangleData} id='age'>
                             <button placeholder='Edad: '/>
                             <label className="m-2 font-bold">Edad: </label>
-                            <input className="m-2 input p-1" type="number"></input>
+                            <input className="m-2 input p-1 input" type="number"></input>
                         </div>
                         <div className={styles.rectangleData} id='e-mail'>
                             <button placeholder='Correo electronico: '/>
                             <label className="m-2 font-bold">Correo electrónico: </label>
-                            <input className="m-2 input p-1" type="email"></input>
+                            <input className="m-2 input p-1 input" type="email"></input>
                         </div>
                         <div className={styles.rectangleData} id='phoneNumber'>
                             <button placeholder='Numero telefonico: '/>
                             <label className="m-2 font-bold">Número telefónico: </label>
-                            <input className="m-2 input p-1" type="number"></input>
+                            <input className="m-2 input p-1 input" type="number"></input>
                         </div>
                         <div className={styles.rectangleData} id='price'>
                             <button placeholder='Precio: '/>
                             <label className="m-2 font-bold">Precio ($): </label>
-                            <input className="m-2 input p-1" type="number"></input>
+                            <input className="m-2 input p-1 input" type="number"></input>
                         </div>
                         <div className={styles.rectangleData} id='specialities'>
                             <button placeholder='Especialidades: '/>
                             <label className="m-2 font-bold">Especialidades: </label>
-                            <input className="m-2 input p-1" type="text"></input>
+                            <input className="m-2 input p-1 input" type="text"></input>
                         </div>
                         <button
                             className="bg-blue-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4"
--- src/pages/ProfilePage/ProfilePacient.jsx
+++ src/pages/ProfilePage/ProfilePacient.jsx
@@ -13,31 +13,31 @@ function ProfilePacient() {
             <div className="Datos">
                 <img className={styles.profileImage} src="{perfil}" alt="Imagen de perfil"></img> 
                 <div className="datosEscritos">
-                    <form>
+                    <form className='form'>
                         <div className={styles.rectangleData} id='name'>
                             <button placeholder='Nombre: '/> 
                             <label className="m-2 font-bold">Nombre: </label>
-                            <input className="m-2 input p-1" type="text"></input>
+                            <input className="m-2 input p-1 input" type="text"></input>
                         </div>
                         <div className={styles.rectangleData} id='lastname'>
                             <button placeholder='Apellido: '/> 
                             <label className="m-2 font-bold">Apellido: </label>
-                            <input className="m-2 input p-1" type="text"></input>
+                            <input className="m-2 input p-1 input" type="text"></input>
                         </div>
                         <div className={styles.rectangleData} id='age'>
                             <button placeholder='Edad: '/>
                             <label className="m-2 font-bold">Edad: </label>
-                            <input className="m-2 input p-1" type="number"></input>
+                            <input className="m-2 input p-1 input" type="number"></input>
                         </div>
                         <div className={styles.rectangleData} id='e-mail'>
                             <button placeholder='Correo electronico: '/>
                             <label className="m-2 font-bold">Correo electrónico: </label>
-                            <input className="m-2 input p-1" type="email"></input>
+                            <input className="m-2 input p-1 input" type="email"></input>
                         </div>
                         <div className={styles.rectangleData} id='phoneNumber'>
                             <button placeholder='Numero telefonico: '/>
                             <label className="m-2 font-bold">Número telefónico: </label>
-                            <input className="m-2 input p-1" type="number"></input>
+                            <input className="m-2 input p-1 input" type="number"></input>
                         </div>
                         <button
                             className="bg-blue-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4"
