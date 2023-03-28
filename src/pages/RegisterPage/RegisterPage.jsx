3eef54d374a7a53cc53147b47bb5f0100d785168
RoyMartin <ranseartin@gmail.com>
2023-03-28 00:29:06 -0400

    cambio de link de imagen default

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -16,7 +16,7 @@ function RegisterPage() {
     name: "",
     email: "",
     password: "",
-    photoURL: "gs://afterglow-c7ada.appspot.com/Perfil_generico.png",
+    photoURL: "https://firebasestorage.googleapis.com/v0/b/afterglow-c7ada.appspot.com/o/Perfil_generico.png?alt=media&token=8be56578-f784-4935-b95b-62a3d3759748",
   });
 
   const handleSignWithGoogle = async () => {
